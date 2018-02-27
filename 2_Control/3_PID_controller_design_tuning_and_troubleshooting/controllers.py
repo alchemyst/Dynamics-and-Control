c00802c5160c9b14a3426be4334992ba86890ae6
Carl Sandrock <carl.sandrock@up.ac.za>
2018-02-27 13:13:43 +0200

    Move minimal integral measures to Theme 3

--- 2_Control/3_PID_controller_design_tuning_and_troubleshooting/controllers.py
+++ 2_Control/3_PID_controller_design_tuning_and_troubleshooting/controllers.py
@@ -16,14 +16,15 @@ class PController:
 class PIController:
     def __init__(self, K_c, tau_I, bias):
         self.G = scipy.signal.lti([K_c * tau_I, K_c], [tau_I, 0])
+        self.Gss = self.G.to_ss()
 
-        self.change_state(numpy.zeros((self.G.A.shape[0], 1)))
+        self.change_state(numpy.zeros((self.Gss.A.shape[0], 1)))
         self.bias = self.output = bias
         self.y = self.bias
 
     def change_input(self, t, u):
-        self.y = self.G.C.dot(self.x) + self.G.D.dot(u) + self.bias
-        self.derivative = self.dxdt = self.G.A.dot(self.x) + self.G.B.dot(u)
+        self.y = self.Gss.C.dot(self.x) + self.Gss.D.dot(u) + self.bias
+        self.derivative = self.dxdt = self.Gss.A.dot(self.x) + self.Gss.B.dot(u)
         self.output = self.y[0, 0]
 
     def change_state(self, x):
@@ -35,14 +36,15 @@ class PIDController:
         self.G = scipy.signal.lti([K_c*alpha_f*tau_D*tau_I + K_c*tau_D*tau_I,
                                    K_c*alpha_f*tau_D + K_c*tau_I, K_c],
                                   [alpha_f*tau_D*tau_I, tau_I, 0.0])
+        self.Gss = self.G.to_ss()
 
-        self.change_state(numpy.zeros((self.G.A.shape[0], 1)))
+        self.change_state(numpy.zeros((self.Gss.A.shape[0], 1)))
         self.bias = self.output = bias
         self.y = self.bias
 
     def change_input(self, t, u):
-        self.y = self.G.C.dot(self.x) + self.G.D.dot(u) + self.bias
-        self.derivative = self.dxdt = self.G.A.dot(self.x) + self.G.B.dot(u)
+        self.y = self.Gss.C.dot(self.x) + self.Gss.D.dot(u) + self.bias
+        self.derivative = self.dxdt = self.Gss.A.dot(self.x) + self.Gss.B.dot(u)
         self.output = self.y[0, 0]
 
     def change_state(self, x):
