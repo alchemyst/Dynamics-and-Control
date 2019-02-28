fd52d4fed792e795b877350b68d200cdba9dc6da
Carl Sandrock <carl.sandrock@up.ac.za>
2019-02-28 06:32:50 +0200

    Bump version and fix formatting

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="tbcontrol",
-    version="0.0.4",
+    version="0.0.5",
     author="Carl Sandrock",
     author_email="carl.sandrock@gmail.com",
     description="Textbook Control Problem package",
