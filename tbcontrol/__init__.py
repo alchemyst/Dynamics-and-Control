1e7b4e462d6525f1e35092f2ca9a8c497a87e794
Carl Sandrock <carl.sandrock@up.ac.za>
2019-03-24 15:59:45 +0200

    Add version checking function

--- tbcontrol/__init__.py
+++ tbcontrol/__init__.py
@@ -1,2 +1,14 @@
 from .version import __version__
 
+class VersionError(Exception):
+    pass
+
+def expectversion(version):
+    import packaging.version
+
+    currentversion = packaging.version.parse(__version__)
+    expectedversion = packaging.version.parse(version)
+
+    if currentversion < expectedversion:
+        raise VersionError(f"Please upgrade tbcontrol to at least version "
+                           f"{version} - you have {__version__}.")
--- tbcontrol/version.py
+++ tbcontrol/version.py
@@ -1,1 +1,3 @@
-__version__ = "0.0.8"
+# This file is designed to be `exec`ed, don't do too much here
+
+__version__ = "0.1.0"
